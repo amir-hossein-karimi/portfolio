5ec29b2bc289543ee62ffb781916da1dc4afbfbc
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-08-29 22:40:49 -0700

    create dark mode

--- src/script.js
+++ src/script.js
@@ -7,7 +7,30 @@ const listItems = ["item1", "item2", "item3", "item4"];
 
 listItems.forEach((item) => {
   const listItem = document.createElement("ol");
-  listItem.classList.add(style.redBackground);
+  listItem.classList.add(style.redBackground, "p-2");
   listItem.innerText = item;
   listEl.appendChild(listItem);
 });
+
+const toggleMode = {
+  dark: "light",
+  light: "dark",
+};
+
+let mode = localStorage.getItem("theme") || "light";
+console.log(mode);
+const darkModeButtonContainer = document.getElementById("darkMode");
+const button = document.createElement("button");
+const html = document.getElementsByTagName("html");
+html[0].className = mode;
+
+button.innerText = toggleMode[mode];
+button.className =
+  "p-4 text-white bg-slate-800 dark:text-slate-800 dark:bg-white";
+button.onclick = () => {
+  html[0].className = toggleMode[mode];
+  localStorage.setItem("theme", toggleMode[mode]);
+  mode = toggleMode[mode];
+  button.innerText = toggleMode[mode];
+};
+darkModeButtonContainer.appendChild(button);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./build/**/*.{html,js}", "./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
