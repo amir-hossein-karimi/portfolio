d21509bf4427d4f27d6a0277b960a685641b968d
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-08-29 22:50:30 -0700

    change on tailwind config

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import "./assets/styles/global.css";
-import style from "./assets/styles/style.scss";
+// import style from "./assets/styles/style.scss";
 
 const listEl = document.getElementById("list");
 
@@ -7,7 +7,7 @@ const listItems = ["item1", "item2", "item3", "item4"];
 
 listItems.forEach((item) => {
   const listItem = document.createElement("ol");
-  listItem.classList.add(style.redBackground, "p-2");
+  listItem.classList.add("p-2");
   listItem.innerText = item;
   listEl.appendChild(listItem);
 });
@@ -18,7 +18,6 @@ const toggleMode = {
 };
 
 let mode = localStorage.getItem("theme") || "light";
-console.log(mode);
 const darkModeButtonContainer = document.getElementById("darkMode");
 const button = document.createElement("button");
 const html = document.getElementsByTagName("html");
@@ -27,6 +26,7 @@ html[0].className = mode;
 button.innerText = toggleMode[mode];
 button.className =
   "p-4 text-white bg-slate-800 dark:text-slate-800 dark:bg-white";
+
 button.onclick = () => {
   html[0].className = toggleMode[mode];
   localStorage.setItem("theme", toggleMode[mode]);
