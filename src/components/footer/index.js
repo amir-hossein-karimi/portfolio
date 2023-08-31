cf92e38ae5905db0ab6ad94fd6ba711036522ff0
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-08-30 22:01:49 -0700

    add footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,7 @@
+import { insertElement } from "../../utils/insertElement";
+import { footerTemplate } from "./template";
+
+// ==== insert footer
+const footerNode = document.createElement("footer");
+footerNode.innerHTML = footerTemplate;
+insertElement(document.getElementById("root"), footerNode, "after");
---
+++ src/components/footer/template.js
@@ -0,0 +1,3 @@
+export const footerTemplate = `
+    <div class="bg-red-600 h-12 p-4 text-white">footer</div>
+`;
