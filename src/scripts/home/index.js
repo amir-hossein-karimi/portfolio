5af34cdc4ac6183249fd98baa4bb1ab8251643a9
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-08-30 22:01:11 -0700

    add some div to home for test

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,1 +1,8 @@
-console.log(document.getElementById("root"));
+const root = document.getElementById("root");
+
+root.innerHTML = `
+    <div>
+        <div class="h-96">home1</div>
+        <div class="h-96">home2</div>
+    </div>
+`;
