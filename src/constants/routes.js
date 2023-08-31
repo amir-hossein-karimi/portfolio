1f5e5af9fcffe68f921da3f709a98f6baefc99c6
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-08-30 21:59:25 -0700

    add routes

---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+const validRoutes = [
+  { route: "home", hasHeader: true, hasFooter: true },
+  { route: "about", hasHeader: false, hasFooter: false },
+];
+
+module.exports = {
+  validRoutes,
+};
