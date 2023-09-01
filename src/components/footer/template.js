a80db3b160078acb87c93a412098638bfeef7d76
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-01 06:58:16 -0700

    change footer and add font

--- src/components/footer/template.js
+++ src/components/footer/template.js
@@ -1,3 +1,15 @@
 export const footerTemplate = `
-    <div class="bg-red-600 h-12 p-4 text-white">footer</div>
+    <div class="flex flex-col justify-center items-center gap-10 px-2 py-6 border-t-2 border-gray-700">
+        <div class="flex gap-12">
+            <div>h</div>
+            <div>a</div>
+            <div>c</div>
+        </div>
+
+        <div class="flex gap-6">
+            <div>i</div>
+            <div>l</div>
+            <div>t</div>
+        </div>
+    </div>
 `;
--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,12 +1,7 @@
-import { linearText } from "../../components/linearText/template";
-
 const root = document.getElementById("root");
 
 root.innerHTML = `
     <div>
-        <div class="h-96">
-            ${linearText({ text: "this is test" })}
-        </div>
-        <div class="h-96">home2</div>
+        home
     </div>
 `;
