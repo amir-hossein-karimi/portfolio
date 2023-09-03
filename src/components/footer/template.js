b60174e7219de6a6b15b3c1d3d2d7b126693d059
amir hossein karimi <a.hosseinkarimi82@gmail.com>
2023-09-03 11:41:58 +0330

    create footer ui

--- src/components/footer/template.js
+++ src/components/footer/template.js
@@ -1,15 +1,30 @@
 export const footerTemplate = `
     <div class="flex flex-col justify-center items-center gap-10 px-2 py-6 border-t-2 border-gray-700">
-        <div class="flex gap-12">
-            <div>h</div>
-            <div>a</div>
-            <div>c</div>
+        <div class="flex sm:gap-12 gap-6">
+            <div class="flex items-center sm:gap-2 gap-1">
+                <img src="../../assets/images/home.svg" alt="home" width="20" height="20">
+                <p class="text-sm">Home</p>
+            </div>
+            <div class="flex items-center sm:gap-2 gap-1">
+                <img src="../../assets/images/user.svg" alt="user" width="20" height="20">
+                <p class="text-sm">About me</p>
+            </div>
+            <div class="flex items-center sm:gap-2 gap-1">
+                <img src="../../assets/images/phone.svg" alt="contact" width="20" height="20">
+                <p class="text-sm">Contact</p>
+            </div>
         </div>
 
-        <div class="flex gap-6">
-            <div>i</div>
-            <div>l</div>
-            <div>t</div>
+        <div class="flex items-center gap-6">
+            <a href="https://www.linkedin.com/in/amir-hossein-karimi-664a89237" target="_blank" class="w-8 h-8 bg-gray-700 rounded-[50%] flex justify-center items-center">
+                <img src="../../assets/images/inLogo.png" alt="in" width="16" height="16">
+            </a>
+            <a href="https://github.com/amir-hossein-karimi" target="_blank" class="w-8 h-8 bg-gray-700 rounded-[50%] flex justify-center items-center">
+                <img src="../../assets/images/githubLogo.jpg" alt="in" width="22" height="22">
+            </a>
+            <a href="mailto:a.hosseinkarimi82@gmail.com" target="_blank" class="w-8 h-8 bg-gray-700 rounded-[50%] flex justify-center items-center">
+                <img src="../../assets/images/mailLogo.png" alt="in" width="16" height="16">
+            </a>
         </div>
     </div>
 `;
