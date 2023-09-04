2ab57c470874f9500c9f6b4413ca642dc5da47d6
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-04 11:40:16 -0700

    remove useless functrion

--- src/utils/sleep.js
+++ src/utils/sleep.js
@@ -5,18 +5,3 @@ export const sleep = (timer = 1_000) => {
     }, timer);
   });
 };
-
-export const isPostalCodeValid = (postalCode = "") => {
-  const pc = postalCode.toString();
-  if (!isNumeric(pc)) return false;
-
-  return (
-    pc.length === 10 &&
-    !pc.slice(0, 5).includes("2") &&
-    !pc.slice(0, 5).includes("0") &&
-    pc[4] !== "5" &&
-    pc[5] !== "0" &&
-    pc.slice(-4) !== "0000" &&
-    !pc.split("").every((char) => char === pc[0])
-  );
-};
