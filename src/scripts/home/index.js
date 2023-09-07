4c5ff2b0ccba34e710195e2b7a6997359a36ac31
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-07 06:56:09 -0700

    feat: create time line

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,7 +1,13 @@
+import "./styles.css";
+
 const root = document.getElementById("root");
 
 root.innerHTML += `
-    <div id="home">
-       test
+    <div class="home">
+       <div class="time-line"></div>
+
+       <div class="grow">
+            <div class="start">&lt;Start /&gt; <span class="start-bullet"></span></div>
+       </div>
     </div>
 `;
