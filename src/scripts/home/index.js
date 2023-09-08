0f6e943e8e3da3ef143e1f92a521210599cec72b
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-07 23:56:23 -0700

    update: chunk home components

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,5 +1,5 @@
-import { glitchTextTemplate } from "../../components/glitchText/template";
 import "./styles.css";
+import HomeStartTemplate from "../../components/Home/Start/template";
 
 const root = document.getElementById("root");
 
@@ -8,16 +8,7 @@ root.innerHTML += `
        <div class="time-line"></div>
 
        <div class="grow">
-            <div class="start-container">
-                <div class="start">&lt;Start /&gt; <span class="start-bullet"></span></div>
-
-                <h1 class="font-bold text-xl sm:text-2xl md:text-3xl">Hi, my name is <strong class="text-secondary-dark">Amir Hossein Karimi</strong></h1>
-                <h1 class="font-bold text-xl sm:text-2xl md:text-3xl">i am a ${glitchTextTemplate(
-                  {
-                    text: "fullstack developer",
-                  }
-                )}</h1>
-            </div>
+            ${HomeStartTemplate}
        </div>
     </div>
 `;
