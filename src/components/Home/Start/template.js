927684e468f3d57702bd87020586800aa853fd83
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-09-08 00:12:23 -0700

    update: move styles to classes

--- src/components/Home/Start/template.js
+++ src/components/Home/Start/template.js
@@ -5,12 +5,10 @@ const HomeStartTemplate = `
     <div class="start-container">
         <div class="start">&lt;Start /&gt; <span class="start-bullet"></span></div>
 
-        <h1 class="font-bold text-xl sm:text-2xl md:text-3xl">Hi, my name is <strong class="text-secondary-dark">Amir Hossein Karimi</strong></h1>
-        <h1 class="font-bold text-xl sm:text-2xl md:text-3xl">i am a ${glitchTextTemplate(
-          {
-            text: "fullstack developer",
-          }
-        )}</h1>
+        <h1 class="heading">Hi, my name is <strong class="text-secondary-dark">Amir Hossein Karimi</strong></h1>
+        <h1 class="heading">i am a ${glitchTextTemplate({
+          text: "fullstack developer",
+        })}</h1>
     </div>
 `;
 
