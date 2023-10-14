26519383840cf6a19747005d6180a58d0d54f7a6
Amirhossein Karimi <a.karimi@ebcom.ir>
2023-10-14 16:51:32 +0330

    projects start

--- src/components/Home/Projects/template.js
+++ src/components/Home/Projects/template.js
@@ -1,9 +1,12 @@
+import "./styles.css";
 import { projects } from "../../../constants/projects";
 import { hoverCardTemplate } from "../../hoverCard/template";
 
 const projectsTemplate = () => {
   return `
-    <div>
+    <div class="relative">
+        <div class="projects-title">&lt;Projects /&gt; <span class="projects-bullet"></span></div>
+
         ${projects.map((project) => `${hoverCardTemplate(project)}`).join("")}
     </div>
 `;
